cfe8b08b932d5c55db254ac8971e0b2a870004f5
f-almeidac <franciele.almeidaac@gmail.com>
2026-09-15 22:59:26 -0300

    fix: alterei a logica da conta do valor

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ funtion calcularTotal (itens){
     let total = 0
 
     for (let i = 0; i < itens.length; i++){
-        total += itens[i].preco * 1.1
+        total += itens[i].preco
     }
 
     return total
